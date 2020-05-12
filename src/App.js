b4dfa0f8c0ce7eb29b970b57b1c35f6d8d14e7a0
DevCarlosU <c.carlos.umana@gmail.com>
2020-05-12 15:21:03 -0500

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import history from './history'
+import { Router, Route, Switch } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+
 export const App = () => {
   return (
-    <div className='App'>
-      <h2>Monoku test</h2>
-    </div>
+    <Router history={history}>
+      <Route
+        render={({ location }) => (
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        )}
+      />
+    </Router>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Monoku app</h2>
+    </div>
+  )
+}
