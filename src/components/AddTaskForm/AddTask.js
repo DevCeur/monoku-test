7c2569c4541cfc19764488df7ff5016602d8fc25
DevCarlosU <c.carlos.umana@gmail.com>
2020-05-12 19:24:03 -0500

    Better way of add tasks ready

--- src/components/AddTaskForm/AddTask.js
+++ src/components/AddTaskForm/AddTask.js
@@ -1,50 +1,73 @@
 import React, { useState } from 'react'
 
+import { TasksContext } from '../../context/TasksContext'
 import { Form, Input } from './AddTaskForm'
 
 export const AddTask = () => {
   const [text, setText] = useState({ text: '' })
   const [showButton, setShowButton] = useState(false)
+  const [error, setError] = useState('')
 
-  const createTask = e => {
-    e.preventDefault()
+  const handleShowButton = e => {
+    if (e.target.value === '') {
+      setShowButton(false)
+      setText({ text: e.target.value })
+    } else {
+      setShowButton(true)
+      setText({ text: e.target.value })
+    }
+  }
+
+  const createTaskApi = async () => {
     const API_ADD_TASK =
       'https://monoku-tasks.herokuapp.com/jtxfoXn2me1c7Tj7B8wn/add'
 
     if (text.text === '') {
-      return null
+      setError('El campo no debe estar vacio')
     } else {
-      fetch(API_ADD_TASK, {
+      const response = await fetch(API_ADD_TASK, {
         method: 'POST',
         body: JSON.stringify(text),
         headers: {
           'Content-Type': 'application/json'
         }
       })
-        .then(res => res.json())
-        .catch(error => console.log('error:', error))
-        // eslint-disable-next-line no-restricted-globals
-        .then(response => location.reload())
+      const json = response.json()
+      return json
     }
   }
 
   return (
-    <Form onSubmit={createTask}>
-      <Input
-        type='text'
-        placeholder='Ej: Comporar tomates'
-        value={text.text}
-        onClick={() => setShowButton(!showButton)}
-        onChange={e => setText({ text: e.target.value })}
-      />
-      {showButton ? (
-        <Input
-          type='submit'
-          submit='true'
-          value='Add task'
-          onSubmit={createTask}
-        />
-      ) : null}
-    </Form>
+    <TasksContext.Consumer>
+      {({ createTask }) => {
+        const createNewTask = e => {
+          e.preventDefault()
+          createTaskApi().then(response => {
+            createTask({ ...response })
+          })
+        }
+
+        return (
+          <Form onSubmit={createNewTask}>
+            <p>{error}</p>
+            <Input
+              type='text'
+              placeholder='Ej: Comporar tomates'
+              value={text.text}
+              onFocus={() => setShowButton(true)}
+              onChange={handleShowButton}
+            />
+            {showButton ? (
+              <Input
+                type='submit'
+                submit='true'
+                value='Add task'
+                onSubmit={createNewTask}
+              />
+            ) : null}
+          </Form>
+        )
+      }}
+    </TasksContext.Consumer>
   )
 }
--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -11,18 +11,19 @@ export const TasksContextProvider = ({ children }) => {
   const API_TASKS =
     'https://monoku-tasks.herokuapp.com/jtxfoXn2me1c7Tj7B8wn/all'
 
+  const getTasks = async () => {
+    const response = await fetch(API_TASKS)
+    const json = await response.json()
+
+    return json
+  }
+
   useEffect(() => {
     setStatus({
       loading: true,
       error: false,
       data: []
     })
-    const getTasks = async () => {
-      const response = await fetch(API_TASKS)
-      const json = await response.json()
-
-      return json
-    }
 
     getTasks().then(data =>
       setStatus({ loading: false, error: false, data: data })
@@ -44,8 +45,20 @@ export const TasksContextProvider = ({ children }) => {
     })
   }
 
+  const createTask = task => {
+    setStatus({
+      loading: true,
+      error: false,
+      data: []
+    })
+
+    getTasks().then(data =>
+      setStatus({ loading: false, error: false, data: data })
+    )
+  }
+
   return (
-    <TasksContext.Provider value={{ status, updateTask }}>
+    <TasksContext.Provider value={{ status, updateTask, createTask }}>
       {children}
     </TasksContext.Provider>
   )
